64f19c03aee8fe467d5efb0e0b5354b1b316490c
Eduardo <eduardolustosa26@gmail.com>
2024-11-12 12:12:45 -0300

    feat: prisma schema

---
+++ src/env/index.ts
@@ -0,0 +1,17 @@
+import 'dotenv/config'
+import { z } from 'zod'
+
+const envSchema = z.object({
+  NODE_ENV: z.enum(['dev', 'production', 'test']).default('production'),
+  PORT: z.coerce.number().default(3000),
+})
+
+const _env = envSchema.safeParse(process.env)
+
+if (!_env.success) {
+  console.error('Invalid environment variables', _env.error.format())
+  throw new Error('Invalid environment variables')
+}
+
+const env = _env.data
+export default env
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,26 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import env from './env'
+import { zValidator } from '@hono/zod-validator'
+import { z } from 'zod'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.post(
+  '/product',
+  zValidator(
+    'json',
+    z.object({
+      title: z.string().min(1),
+      ownerId: z.string().min(1),
+    }),
+  ),
+  (c) => {
+    return c.text('Hello Hono!')
+  },
+)
 
-const port = 3000
+const port = env.PORT
 console.log(`Server is running on http://localhost:${port}`)
 
 serve({
